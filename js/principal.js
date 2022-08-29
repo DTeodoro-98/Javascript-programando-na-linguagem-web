ceafd6d7373f88c01126bff775277c0d5979e02f
Douglas <dougynha1907@hotmail.com>
2022-08-29 18:30:47 -0300

    Operadores Lógicos

--- js/principal.js
+++ js/principal.js
@@ -9,7 +9,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
-
 var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido.");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido.";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválido.");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválido.";
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
